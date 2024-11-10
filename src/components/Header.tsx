27000fe4afe144f5718e99e9c1ec529cbef278d9
moinuddin <moinminhal893@gmail.com>
2024-11-10 08:40:08 +0500

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="h-[134px] bg-[#A29875]">
+    <div className="w-[439px] h-[94px] text-[75px] font-normal text-[#FFFFFF] leading-[93.75px] p-4 mx-4">MANZZARI</div>
+  </div>
+  )
+}
